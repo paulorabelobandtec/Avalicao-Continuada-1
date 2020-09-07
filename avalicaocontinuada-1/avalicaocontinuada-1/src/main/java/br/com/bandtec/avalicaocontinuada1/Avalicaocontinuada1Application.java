8368086d8c9d590b0493e5e90d4c38d636501887
paulorabelobandtec <53615538+paulorabelobandtec@users.noreply.github.com>
2020-09-07 14:14:48 -0300

    Add files via upload

---
+++ avalicaocontinuada-1/avalicaocontinuada-1/src/main/java/br/com/bandtec/avalicaocontinuada1/Avalicaocontinuada1Application.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.avalicaocontinuada1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avalicaocontinuada1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Avalicaocontinuada1Application.class, args);
+	}
+
+}
